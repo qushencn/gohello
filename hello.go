b2b18c122426e218a2d2a3df05bed570015a450f
qushen <qushencn@qq.com>
2022-01-15 13:22:23 +0800

    更新了函数

--- hello.go
+++ hello.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // 一个用于演示如何发布自定义包的示例项目。
 
-// SayHi 一个打招呼的函数
-func SayHi(name string) {
+// Hi 一个打招呼的函数
+func Hi(name string) {
 	fmt.Printf("你好%s，我是qushencn。很高兴你可以调用我的github仓库~\n", name)
 }
